735fd8fa3d971205af2298dde2125785026dce78
elkrlos10 <www.kliche10@gmail.com>
2018-06-01 13:41:02 -0500

    Inicio Proyecto

---
+++ paises/Paises/Paises/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Paises
+{
+    using Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+	{
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new LoginPage();
+        }
+
+        #endregion
+ 
+        #region Metodos
+        protected override void OnStart()
+        {
+            // Cuando la app de ejecuta
+        }
+
+        protected override void OnSleep()
+        {
+            //Dormir la app
+        }
+
+        protected override void OnResume()
+        {
+            // Despertar la app
+        } 
+        #endregion
+    }
+}
