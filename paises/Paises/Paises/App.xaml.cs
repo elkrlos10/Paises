b503d357e122a479dd1d80372bf1a94b572e8b78
elkrlos10 <www.kliche10@gmail.com>
2018-06-05 12:39:30 -0500

    Cambios Login

--- paises/Paises/Paises/App.xaml.cs
+++ paises/Paises/Paises/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Paises
         {
             InitializeComponent();
 
-            this.MainPage = new LoginPage();
+            this.MainPage = new NavigationPage(new LoginPage());
         }
 
         #endregion
