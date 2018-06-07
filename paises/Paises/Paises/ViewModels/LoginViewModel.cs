5962568cd03df820c44f12d8a92db722d0ad913c
elkrlos10 <www.kliche10@gmail.com>
2018-06-07 05:52:29 -0500

    avances

---
+++ paises/Paises/Paises/ViewModels/LoginViewModel.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Paises.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Paises.Views;
+    using System.ComponentModel;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModel
+    {
+        //Evento de la interfaz INotifyPropertyChanged para refrescar las vistas
+        #region Eventos
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        #region Atributos
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        private string email;
+        #endregion
+
+        #region Propiedades
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+        public bool IsRemembered { get; set; }
+        //Por medio de los atributos se deben actualizar las propiedades en tiempo de ejecucíon en la aplicación
+        public string Password
+        {
+            get { return this.password;}
+            set { SetValue(ref password, value); }
+        }
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref isRunning, value); }
+        }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref isEnabled, value); }
+
+        }
+        #endregion
+
+        #region Contructor
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.isEnabled = true;
+        }
+        #endregion
+
+        //Comandos
+        #region Commands 
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+
+        private async void Login()
+        {
+            //Validar si es null o vacio la propiedad
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                      "Error"
+                    , "Ingrese por favor el Email"
+                    , "Aceptar");
+
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                      "Error"
+                    , "Ingrese por favor la Contraseña"
+                    , "Aceptar");
+
+                return;
+            }
+
+            if (this.Email != "a@a.com" || this.Password != "123")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                      "Error"
+                    , "Email o Contraseña Incorrectas"
+                    , "Aceptar");
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            this.Email = string.Empty;
+            this.Password = string.Empty;
+
+            MainViewModel.GetInstance().Paises = new PaisesViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new PaisesPage());
+
+        }
+
+        #endregion
+    }
+}
--- paises/Paises/Paises/ViewModels/MainViewModel.cs
+++ paises/Paises/Paises/ViewModels/MainViewModel.cs
@@ -6,6 +6,30 @@ namespace Paises.ViewModels
 {
     public class MainViewModel
     {
-       
+        #region ViewModel
+
+        public LoginViewModel Login { get; set; }
+        public PaisesViewModel Paises { get; set; }
+        #endregion
+
+        #region Contructores
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+        public static MainViewModel GetInstance()
+        {
+            if (instance== null)
+            {
+                return new MainViewModel();
+            }
+            return instance;
+        }
+        #endregion
     }
 }
